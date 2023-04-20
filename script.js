f14e5a19b35374f0c1d7b0ddaad4d075b66d9007
EminovKamran <85683610+EminovKamran@users.noreply.github.com>
2023-04-21 03:05:45 +0400

    refactor: icon image

--- script.js
+++ script.js
@@ -4,20 +4,8 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 'auto',
     spaceBetween: 15,
 
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-
-    // Navigation arrows
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
 });
