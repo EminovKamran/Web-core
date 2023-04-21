1570a0b1270d81543fc1afa43f93fb50bd4c1c71
EminovKamran <85683610+EminovKamran@users.noreply.github.com>
2023-04-21 11:46:36 +0400

    init: add swiper

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 'auto',
-    spaceBetween: 15,
+let width = 768;
+let mySwiper = undefined;
+let pagination = document.querySelector('.swiper-pagination');
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+function mobileSlider() {
+    if (window.innerWidth < width && mySwiper === undefined) {
+        mySwiper = new Swiper('.swiper', {
+               direction: 'horizontal',
+                slidesPerView: 'auto',
+               spaceBetween: 15,
+               loop: true,
+               pagination: {
+                   el: '.swiper-pagination',
+                   clickable: true,
+               },
+           });
+        pagination.style.position = 'static';
+    } else if (window.innerWidth > width && mySwiper !== undefined) {
+        mySwiper.destroy();
+        mySwiper = undefined;
+        pagination.style.position = 'absolute';
+    }
+}
+mobileSlider()
+window.addEventListener("resize", mobileSlider)
+
+
